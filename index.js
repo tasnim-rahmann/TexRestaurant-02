11d9360938e05d2f706763504ea5ceaf71b738e5
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-09-10 18:12:05 +0600

    get and post api's for cart,user

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -23,10 +23,60 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
+
+        const userCollection = client.db("tex-rest-db").collection("users");
+        const menuCollection = client.db("tex-rest-db").collection("menu");
+        const reviewCollection = client.db("tex-rest-db").collection("reviews");
+        const cartCollection = client.db("tex-rest-db").collection("carts");
+
+        // User API's
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await userCollection.findOne(query);
+
+            if (existingUser) return res.send({ message: 'user with this email is already exists' });
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+        // Menu Collection
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result);
+        });
+
+        // Review Collection
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        });
+
+        // Cart Collection
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.post('/carts', async (req, res) => {
+            const cartItem = req.body;
+            const result = await cartCollection.insertOne(cartItem);
+            res.send(result);
+        });
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        });
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
